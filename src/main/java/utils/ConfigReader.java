fb8f1dbef4021439830d11bfc0659b1e9a169b50
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-02 19:18:57 +0700

    config project

---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class ConfigReader {
+    Properties properties;
+    public ConfigReader(){
+        properties = new Properties();
+        try{
+            properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    public String getUrl(){
+        return properties.getProperty("url");
+    }
+    public Duration getTimeOut(){
+        return Duration.ofSeconds(Long.parseLong(properties.getProperty("timeout")));
+    }
+}
