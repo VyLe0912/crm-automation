8183fc9d5073186eb9b1d9541abcd4dc6ee262ae
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-04 22:15:32 +0700

    Test case open login fix

--- src/test/java/example/VerifyUserOpenLoginModal.java
+++ src/test/java/example/VerifyUserOpenLoginModal.java
@@ -1,14 +1,18 @@
 package example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import page.LoginPage;
 import utils.ConfigReader;
 
 public class VerifyUserOpenLoginModal {
+    By labelLoginSelector = By.xpath("(//div[@class = 'ibox-title'])[1]");
+
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
@@ -19,6 +23,9 @@ public class VerifyUserOpenLoginModal {
     @Test
     public void verifyLoginFunction() {
         driver.get(configReader.getUrl());
+        String labelLogin = driver.findElement(labelLoginSelector).getText();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(labelLogin, "Login", "Access web failed");
     }
 
     @AfterMethod
