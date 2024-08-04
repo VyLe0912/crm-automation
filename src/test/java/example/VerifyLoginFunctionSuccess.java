6507bbb521f6e73df92422590893419530f62b02
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-04 21:42:47 +0700

    Test case Login function fix

--- src/test/java/example/VerifyLoginFunctionSuccess.java
+++ src/test/java/example/VerifyLoginFunctionSuccess.java
@@ -1,25 +1,37 @@
 package example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import page.LoginPage;
 import utils.ConfigReader;
 
-public class VerifyLoginFunction {
+import java.time.Duration;
+
+public class VerifyLoginFunctionSuccess {
+    By headerLoginSelector = By.xpath("(//strong[@class='font-bold'])[2]");
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
         configReader = new ConfigReader();
         loginPage = new LoginPage(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Test
     public void verifyLoginFunction() {
         driver.get(configReader.getUrl());
+        driver.manage().window().setSize(new Dimension(1378, 744));
         loginPage.login("abc@gmail.com", "VyLe123!");
+        String headerLogin = driver.findElement(headerLoginSelector).getText();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(headerLogin, "Vy", "Login failed");
+        softAssert.assertAll();
     }
 
     @AfterMethod
