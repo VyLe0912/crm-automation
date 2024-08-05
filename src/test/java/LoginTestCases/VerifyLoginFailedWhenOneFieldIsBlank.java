cfd5683fa05daaabbc4b5b68ec0f949c8208e239
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-05 17:59:42 +0700

    TCs Login fail when one field is blank

---
+++ src/test/java/LoginTestCases/VerifyLoginFailedWhenOneFieldIsBlank.java
@@ -0,0 +1,49 @@
+package LoginTestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import page.LoginPage;
+import utils.ConfigReader;
+
+public class VerifyLoginFailedWhenOneFieldIsBlank {
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        configReader = new ConfigReader();
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void VerifyLoginFailedWhenOneFieldIsBlank() {
+        driver.get(configReader.getUrl());
+        loginPage.login("", "VyLe123!");
+        loginPage.waitForTextAppear();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        //Kiểm tra thông báo trường Email hiển thị
+        softAssert.assertEquals(loginPage.emailTextMessage(), "Please enter your email", "Email field error");
+
+        driver.navigate().refresh();
+        loginPage.login("abc@gmail.com","");
+        loginPage.waitForTextAppear();
+
+        //Kiểm tra thông báo trường password hiển thị
+        softAssert.assertEquals(loginPage.passTextMessage(), "Please enter your password", "Password field error");
+
+        softAssert.assertAll();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanUp() {
+        driver.quit();
+    }
+
+    WebDriver driver;
+    ConfigReader configReader;
+    LoginPage loginPage;
+}
