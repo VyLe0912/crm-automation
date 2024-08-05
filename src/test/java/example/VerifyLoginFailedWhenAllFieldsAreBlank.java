aa4bf619a506bbc8ffb68f6de3ea9563eebef87e
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-05 10:31:25 +0700

    Test case open login fix 2

---
+++ src/test/java/example/VerifyLoginFailedWhenAllFieldsAreBlank.java
@@ -0,0 +1,44 @@
+package example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import page.LoginPage;
+import utils.ConfigReader;
+
+public class VerifyLoginFailedWhenAllFieldsAreBlank {
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        configReader = new ConfigReader();
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test
+    public void VerifyLoginFailedWhenAllFieldsAreBlank() {
+        driver.get(configReader.getUrl());
+        loginPage.login("","");
+        loginPage.waitForTextAppear();
+        SoftAssert softAssert = new SoftAssert();
+
+        //Kiểm tra thông báo trường Email hiển thị
+        softAssert.assertEquals(loginPage.emailTextMessage(), "Please enter your email", "Email field error");
+
+        //Kiểm tra thông báo trường Pass hiển thị
+        softAssert.assertEquals(loginPage.passTextMessage(), "Please enter your password", "Password field error");
+
+        softAssert.assertAll();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void cleanUp() {
+        driver.quit();
+    }
+
+    WebDriver driver;
+    ConfigReader configReader;
+    LoginPage loginPage;
+}
