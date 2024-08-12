fbafcb38c32f37e61a5030dbb90abd46a63cc5e0
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-12 20:43:46 +0700

    push page object

---
+++ src/main/java/page/Product/CreateProduct.java
@@ -0,0 +1,37 @@
+package page.Product;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CreateProduct {
+    WebDriver driver;
+
+    By txbProductNameInputSelector;
+    By txbProductPriceInputSelector;
+    By txbProductDiscountInputSelector;
+    By btnSaveProductInputSelector;
+
+    //Messages
+    By getTxtMessProductInputSelector;
+    By getTxtMessProductPriceInputSelector;
+    By getTxtMessProductDiscountInputSelector;
+
+    public CreateProduct(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterProductName() {
+    }
+
+    public void enterProductPrice() {
+    }
+
+    public void enterProductDiscount() {
+    }
+
+    public void clickBtnSave() {
+    }
+
+    public void createProduct() {
+    }
+}
---
+++ src/main/java/page/Product/ProductMenu.java
@@ -0,0 +1,15 @@
+package page.Product;
+
+import org.openqa.selenium.By;
+
+public class ProductMenu {
+    By btnShowAllProductsInputSelector;
+    By btnCreateProduct;
+
+    public void clickBtnShowAllProducts() {
+    }
+
+    public void clickBtnCreateProduct() {
+    }
+
+}
