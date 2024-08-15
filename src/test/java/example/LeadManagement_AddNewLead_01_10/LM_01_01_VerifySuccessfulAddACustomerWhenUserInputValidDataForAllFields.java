0905f1dcee3c802b046c612f33eb5ad60543c110
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-15 16:13:35 +0700

    Fix TCs LM

--- src/test/java/example/LeadManagement_AddNewLead_01_10/LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFields.java
+++ src/test/java/example/LeadManagement_AddNewLead_01_10/LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFields.java
@@ -1,6 +1,7 @@
 package example.LeadManagement_AddNewLead_01_10;
 
 import com.github.javafaker.Faker;
+import example.TestBase;
 import io.qameta.allure.Allure;
 import models.CustomerInFormationForm;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -16,9 +17,7 @@ import page.ShowAllCustomersPage;
 import utils.ConfigReader;
 import java.time.Duration;
 
-public class LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFields {
-    WebDriver driver;
-    ConfigReader configReader;
+public class LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFields extends TestBase {
     LoginPage loginPage;
     ShowAllCustomersPage showAllCustomersPage;
     CreateCustomerPage createCustomerPage;
@@ -33,15 +32,11 @@ public class LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFi
 
     @BeforeMethod
     public void setUp() {
-
-        driver = new ChromeDriver();
-        configReader = new ConfigReader();
+        super.setUp();
         loginPage = new LoginPage(driver);
         showAllCustomersPage = new ShowAllCustomersPage(driver);
         createCustomerPage = new CreateCustomerPage(driver);
         faker = new Faker();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         softAssert = new SoftAssert();
 
         name = faker.name().name();
@@ -53,9 +48,6 @@ public class LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFi
 
     @Test
     public void testLM_01_01() {
-
-        Allure.step("Open CRM website");
-        driver.get(configReader.getUrl());
 
         Allure.step("Login success");
         loginPage.login("abcTrang@gmail.com", "123123");
@@ -78,10 +70,5 @@ public class LM_01_01_VerifySuccessfulAddACustomerWhenUserInputValidDataForAllFi
         softAssert.assertEquals(showAllCustomersPage.getCustomerAddressByIndex(1), address, "The newly created customer is not at the top of the list.");
 
         softAssert.assertAll();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void cleanUp() {
-        driver.quit();
     }
 }
--- src/test/java/example/LeadManagement_AddNewLead_01_10/LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField.java
+++ src/test/java/example/LeadManagement_AddNewLead_01_10/LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField.java
@@ -1,6 +1,7 @@
 package example.LeadManagement_AddNewLead_01_10;
 
 import com.github.javafaker.Faker;
+import example.TestBase;
 import io.qameta.allure.Allure;
 import models.CustomerInFormationForm;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -17,9 +18,8 @@ import utils.ConfigReader;
 
 import java.time.Duration;
 
-public class LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField {
-    WebDriver driver;
-    ConfigReader configReader;
+public class LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField extends TestBase {
+
     LoginPage loginPage;
     ShowAllCustomersPage showAllCustomersPage;
     CreateCustomerPage createCustomerPage;
@@ -34,16 +34,12 @@ public class LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField {
 
     @BeforeMethod
     public void setUp() {
+        super.setUp();
         softAssert = new SoftAssert();
-        driver = new ChromeDriver();
-        configReader = new ConfigReader();
         loginPage = new LoginPage(driver);
         showAllCustomersPage = new ShowAllCustomersPage(driver);
         createCustomerPage = new CreateCustomerPage(driver);
         faker = new Faker();
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         name1 = RandomStringUtils.randomAlphabetic(1);
         name49 = RandomStringUtils.randomAlphabetic(49);
@@ -59,9 +55,6 @@ public class LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField {
 
     @Test
     public void testLM_01_05() {
-
-        Allure.step("Open CRM website");
-        driver.get(configReader.getUrl());
 
         Allure.step("Login success");
         loginPage.login("abcTrang@gmail.com", "123123");
@@ -100,8 +93,4 @@ public class LM_01_05_VerifyCanAddCustomerWhenEnteringValidValueForNameField {
         softAssert.assertAll();
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void cleanUp() {
-        driver.quit();
-    }
 }
