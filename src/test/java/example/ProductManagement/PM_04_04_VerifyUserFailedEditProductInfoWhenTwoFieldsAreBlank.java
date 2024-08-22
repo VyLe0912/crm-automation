de6071c7abaea0e58d08ec47afad191e005df3ed
Vy Le <85723167+VyLe0912@users.noreply.github.com>
2024-08-22 21:47:02 +0700

    add new TC PM_04_04

--- src/test/java/example/ProductManagement/PM_04_04_VerifyUserFailedEditProductInfoWhenTwoFieldsAreBlank.java
+++ src/test/java/example/ProductManagement/PM_04_04_VerifyUserFailedEditProductInfoWhenTwoFieldsAreBlank.java
@@ -3,18 +3,17 @@ package example.ProductManagement;
 import com.github.javafaker.Faker;
 import example.TestBase;
 import io.qameta.allure.Allure;
+import models.ProductForm;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import page.Login.LoginPage;
-import page.Products.CreateProductPage;
 import page.Product.EditProduct;
-import models.ProductForm;
 import page.Product.ShowAllProducts;
+import page.Products.CreateProductPage;
 import page.SideBar.SideBar;
 
-
-public class PM_04_03_VerifyUserFailedEditProductInfoWhenOneFieldIsBlank extends TestBase {
+public class PM_04_04_VerifyUserFailedEditProductInfoWhenTwoFieldsAreBlank extends TestBase {
     @BeforeMethod
     public void setUp() {
         super.setUp();
@@ -32,7 +31,7 @@ public class PM_04_03_VerifyUserFailedEditProductInfoWhenOneFieldIsBlank extends
     }
 
     @Test
-    public void VerifyUserFailedEditProductInfoWhenOneFieldIsBlank() {
+    public void VerifyUserFailedEditProductInfoWhenTwoFieldsAreBlank() {
 
         Allure.step("Login function");
         loginPage.loginWithDefaultAccount();
@@ -44,25 +43,29 @@ public class PM_04_03_VerifyUserFailedEditProductInfoWhenOneFieldIsBlank extends
 
         createProduct.deleteAllTextBox();
 
-        Allure.step("Name field is blank");
-        productForm = new ProductForm("", price, discount);
+        Allure.step("Name and price fields are blank");
+        productForm = new ProductForm("", "", discount);
         editProduct.editProduct(productForm);
 
         softAssert.assertEquals(createProduct.getMessProName(), "Please enter product name", "Error name");
-
-        createProduct.deleteAllTextBox();
-
-        Allure.step("price field is blank");
-        productForm = new ProductForm(name, "", discount);
-        editProduct.editProduct(productForm);
-
         softAssert.assertEquals(createProduct.getMessProPrice(), "Please enter price", "Error price");
 
         createProduct.deleteAllTextBox();
 
-        Allure.step("discount field is blank");
-        productForm = new ProductForm(name, price, "");
+        Allure.step("Price and discount fields are blank");
+        productForm = new ProductForm(name, "", "");
         editProduct.editProduct(productForm);
+
+        softAssert.assertEquals(createProduct.getMessProName(), "Please enter product name", "Error name");
+        softAssert.assertEquals(createProduct.getMessProDiscount(), "Please enter discount", "Error discount");
+
+        createProduct.deleteAllTextBox();
+
+        Allure.step("Name and discount fields are blank");
+        productForm = new ProductForm("", price, "");
+        editProduct.editProduct(productForm);
+
+        softAssert.assertEquals(createProduct.getMessProName(), "Please enter product name", "Error name");
         softAssert.assertEquals(createProduct.getMessProDiscount(), "Please enter discount", "Error discount");
 
         softAssert.assertAll();
